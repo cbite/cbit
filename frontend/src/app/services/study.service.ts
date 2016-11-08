ec09972f737f49b3376b96a2bfe96928f0d544a4
Patrick Varilly <patrick.varilly@dataminded.be>
2016-11-08 15:37:51 +0100

    Dead comment

--- frontend/src/app/services/study.service.ts
+++ frontend/src/app/services/study.service.ts
@@ -43,17 +43,6 @@ export interface ManySampleCounts {
 
 @Injectable()
 export class StudyService {
-  /*getStudies(): Promise<Study[]> {
-    //return Promise.resolve(STUDIES);
-    return new Promise<Study[]>(resolve =>
-      setTimeout(resolve, 2000)) // delay 2 seconds
-      .then(() => Promise.resolve(STUDIES));
-  }
-
-  getStudy(id: number): Promise<Study> {
-    return this.getStudies()
-      .then(studies => studies.find(study => study.id === id));
-  }*/
 
   // PUBLIC INTERFACE
   // ================
