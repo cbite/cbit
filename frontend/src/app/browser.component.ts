efbe560848f1a6adf6f0f4bbac70ba8013ad2843
Patrick Varilly <patrick.varilly@dataminded.be>
2016-11-08 15:35:10 +0100

    Proper use of "switchMap" in BrowserComponent to properly ignore stale responses of in-flight backend calls when filters change

--- frontend/src/app/browser.component.ts
+++ frontend/src/app/browser.component.ts
@@ -70,58 +70,61 @@ export class BrowserComponent {
   }
 
   ngOnInit(): void {
-    //this._studyService.getStudies().then(studies => this.studies = studies);
-    this._filtersService.filters.subscribe(filters => this.updateStudies(filters));
+    // Use switchMap to cancel in-flight queries if new filters are applied in the meantime
+    this._filtersService.filters
+      .switchMap(filters => {
+        this.ready = false;
+        return this._studyService.getUnifiedMatchesAsync(filters);
+      })
+      .subscribe(rawMatches => {
+        this.updateMatches(rawMatches);
+        this.ready = true;
+      });
   }
 
-  updateStudies(filters: FiltersState): void {
-    this.ready = false;
-    this._studyService.getUnifiedMatchesAsync(filters).then(rawMatches => {
-      this.matches = rawMatches.sort((a, b) =>
-        a.study._source['STUDY']['Study Researchers Involved']
-          .localeCompare(b.study._source['STUDY']['Study Researchers Involved']));
+  updateMatches(rawMatches: UnifiedMatch[]): void {
+    this.matches = rawMatches.sort((a, b) =>
+      a.study._source['STUDY']['Study Researchers Involved']
+        .localeCompare(b.study._source['STUDY']['Study Researchers Involved']));
 
-      let keys = new Set<string>();
-      this.matches.forEach(studyMatch => {
-        studyMatch.sampleMatches.forEach(sampleMatch => Object.keys(sampleMatch.sample._source).forEach(key => keys.add(key)));
-      });
-      keys.delete('Sample Name');
-      this.sampleKeys = Array.from(keys).sort();
+    let keys = new Set<string>();
+    this.matches.forEach(studyMatch => {
+      studyMatch.sampleMatches.forEach(sampleMatch => Object.keys(sampleMatch.sample._source).forEach(key => keys.add(key)));
+    });
+    keys.delete('Sample Name');
+    this.sampleKeys = Array.from(keys).sort();
 
-      // By default, show matching samples
-      for (let studyMatch of this.matches) {
-        if (!(studyMatch.studyId in this.areSamplesHidden)) {
-          this.areSamplesHidden[studyMatch.studyId] = false;
-        }
+    // By default, show matching samples
+    for (let studyMatch of this.matches) {
+      if (!(studyMatch.studyId in this.areSamplesHidden)) {
+        this.areSamplesHidden[studyMatch.studyId] = false;
       }
+    }
 
-      this.commonKeys = {};
-      for (let studyMatch of this.matches) {
-        let studyCommonKeys = {};
-        if (studyMatch.sampleMatches.length > 0) {
-          let firstSampleMatch = studyMatch.sampleMatches[0];
-          for (let key in firstSampleMatch.sample._source) {
-            studyCommonKeys[key] = firstSampleMatch.sample._source[key];
-          }
+    this.commonKeys = {};
+    for (let studyMatch of this.matches) {
+      let studyCommonKeys = {};
+      if (studyMatch.sampleMatches.length > 0) {
+        let firstSampleMatch = studyMatch.sampleMatches[0];
+        for (let key in firstSampleMatch.sample._source) {
+          studyCommonKeys[key] = firstSampleMatch.sample._source[key];
+        }
 
-          for (let sampleMatch of studyMatch.sampleMatches) {
-            for (let commonKey in studyCommonKeys) {
-              if (!(commonKey in sampleMatch.sample._source) ||
-                (sampleMatch.sample._source[commonKey] !== studyCommonKeys[commonKey])) {
-                delete studyCommonKeys[commonKey];
-              }
+        for (let sampleMatch of studyMatch.sampleMatches) {
+          for (let commonKey in studyCommonKeys) {
+            if (!(commonKey in sampleMatch.sample._source) ||
+              (sampleMatch.sample._source[commonKey] !== studyCommonKeys[commonKey])) {
+              delete studyCommonKeys[commonKey];
             }
           }
         }
-
-        this.commonKeys[studyMatch.studyId] = studyCommonKeys;
       }
 
-      this.numMatchingStudies = this.matches.length;
-      this.numMatchingSamples = this.matches.reduce((soFar, studyMatch) => soFar + studyMatch.sampleMatches.length, 0);
+      this.commonKeys[studyMatch.studyId] = studyCommonKeys;
+    }
 
-      this.ready = true;
-    });
+    this.numMatchingStudies = this.matches.length;
+    this.numMatchingSamples = this.matches.reduce((soFar, studyMatch) => soFar + studyMatch.sampleMatches.length, 0);
   }
 
   distinctKeyValues(studyId: number, sample: Sample): Object {
