3295ac4dfeb4fdcc9e9c3ecd6d47d5a6451698d2
Patrick Varilly <patrick.varilly@dataminded.be>
2016-11-10 10:37:35 +0100

    Remove all mock data from front-end

--- frontend/src/app/common/study.model.ts
+++ frontend/src/app/common/study.model.ts
@@ -1,92 +1,30 @@
-export interface RawInvestigation {
-  "Investigation Identifier"?: string,
-  "Investigation Title"?: string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawOntologySourceReference {
-  "Term Source Description"?: string,
-  "Term Source File"?: string,
-  "Term Source Name"?: string,
-  "Term Source Version"?: string,
-  [propName: string]: any    // Allow anything else as well
-}
-
 export interface RawStudyInfo {
-  "Study Identifier": string,
+  // Only describing the metadata we actually use in the front-end
   "Study Title": string,
-  "Study Description": string,
-  "Study File Name": string,
-  "Study Submission Date"?: string,
-  "Study Public Release Date"?: string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawStudyAssay {
-  "Study Assay File Name": string,
-  "Study Assay Measurement Type": string,
-  "Study Assay Technology Platform": string,
-  "Study Assay Technology Type": string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawStudyContact {
-  "Study Person First Name": string,
-  "Study Person Last Name": string,
-  "Study Person Affiliation"?: string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawStudyDesignDescriptor {
-  "Study Design Type"?: string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawStudyFactor {
-  "Study Factor Name": string,
-  "Study Factor Type": string,
-  [propName: string]: any    // Allow anything else as well
-}
-
-export interface RawStudyProtocol {
-  "Study Protocol Name": string,
-  "Study Protocol Type": string,
-  "Study Protocol Description": string,
+  "Study Researchers Involved": string,
   [propName: string]: any    // Allow anything else as well
 }
 
 export interface RawStudyPublication {
-  "Study Publication Title": string,
-  "Study Publication Author List": string,
+  // Only describing the metadata we actually use in the front-end
   "Study Publication DOI"?: string,
   "Study PubMed ID"?: string,
-  "Study Publication Status"?: string,
-  "Study Publication Status Term Accession Number"?: string,
-  "Study Publication Status Term Source REF"?: string,
   [propName: string]: any    // Allow anything else as well
 }
 
 export interface RawStudy {
-  "INVESTIGATION"?:             RawInvestigation,
-  "ONTOLOGY SOURCE REFERENCE"?: RawOntologySourceReference,
+  // Only describing the metadata we actually use in the front-end
   "STUDY":                      RawStudyInfo,          // 1 study per investigation
-  "STUDY ASSAYS":               RawStudyAssay,  // 1 assay per investigation
-  "STUDY CONTACTS":             Array<RawStudyContact>,
-  "STUDY DESIGN DESCRIPTORS"?:  RawStudyDesignDescriptor,
-  "STUDY FACTORS":              Array<RawStudyFactor>,
-  "STUDY PROTOCOLS":            Array<RawStudyProtocol>,
   "STUDY PUBLICATIONS":         Array<RawStudyPublication>,  // TODO: Enforce array in back-end
   [propName: string]: any    // Allow anything else as well
 }
 
 export interface Study {
   id: string,
-  sampleIds: Array<number>,
   _source: RawStudy
 }
 
 export interface Sample {
   id: number,
-  studyId: string,
   _source: Object
 }
--- frontend/src/app/services/study.service.ts
+++ frontend/src/app/services/study.service.ts
@@ -1,6 +1,5 @@
 import {Study, Sample, RawStudy} from '../common/study.model';
 import {Injectable} from "@angular/core";
-import {STUDIES, SAMPLES} from '../common/mock-studies';
 import * as _ from 'lodash';
 import {FiltersState, EMPTY_FILTERS, SampleFilter, FilterMode} from "./filters.service";
 import { Client as ESClient, SearchResponse as ESSearchResponse } from "elasticsearch";
@@ -101,7 +100,6 @@ export class StudyService {
             if (!study) {
               study = {
                 id: hit._id,
-                sampleIds: [],   // TODO: Drop this field
                 _source: <RawStudy>hit._source
               }
             } else {
@@ -110,7 +108,6 @@ export class StudyService {
           } else if (hit._type === 'sample') {
             samples.push({
               id: i,
-              studyId: studyId,
               _source: hit._source
             });
             i += 1;
@@ -126,6 +123,11 @@ export class StudyService {
   }
 
   getManySampleCountsAsync(filters: FiltersState, categories: string[]): PromiseLike<ManySampleCounts> {
+
+    // Need to be careful when building up counts of values for each category: for each category, we
+    // apply all filters *except* the one being queried (so we get a count of all studies that would be included
+    // if we allow a particular value for this category-subcategory pair)
+
     // 1. Get list of controls needed
     let controlsPromise: PromiseLike<string[]>;
     if (filters.includeControls) {
@@ -236,17 +238,6 @@ export class StudyService {
 
       return result;
     });
-
-    /*return (
-      new Promise<ManySampleCounts>(resolve => setTimeout(resolve, 2000)) // delay 2 seconds
-        .then(() => {
-          let result = {};
-          for (let category of categories) {
-            result[category] = this.getSampleCounts(filters, category);
-          }
-          return result;
-        })
-    )*/
   }
 
   getUnifiedMatchesAsync(filters: FiltersState): PromiseLike<UnifiedMatch[]> {
@@ -337,14 +328,6 @@ export class StudyService {
 
       return Object.values(unifiedMatches);
     });
-
-    /*return (
-
-      new Promise<UnifiedMatch[]>(resolve => setTimeout(resolve, 2000))  // delay 2 seconds
-        .then(() => {
-          return this.getUnifiedMatchesSync(filters);
-        })
-    )*/
   }
 
 
@@ -453,201 +436,5 @@ export class StudyService {
       host: 'http://localhost:9200'
       //,log: 'trace'
     })
-  }
-
-  private getStudies(): Study[] {
-    return STUDIES;
-  }
-
-  private getSamples(): Sample[] {
-    return SAMPLES;
-  }
-
-  private shouldSampleBeExcluded(category: string, sampleFilter: SampleFilter, sample: Sample): boolean {
-
-    var value: string;
-    if (category in sample._source) {
-      value = '' + sample._source[category];  // Force string comparison, yuck!
-    } else {
-      value = NULL_CATEGORY_NAME;
-    }
-
-    switch (sampleFilter.mode) {
-      case FilterMode.AllButThese:
-        return (value in sampleFilter.detail);
-      case FilterMode.OnlyThese:
-        return !(value in sampleFilter.detail);
-    }
-  }
-
-  private forceIncludeInSampleFilters(filters: FiltersState, category: string) {
-    if (category in filters.sampleFilters) {
-
-      let tweakedFilters = _.cloneDeep(filters);
-      delete tweakedFilters.sampleFilters[category];
-      return tweakedFilters;
-    } else {
-      return filters;
-    }
-  }
-
-  private getSampleCounts(filters: FiltersState, category: string): SampleCounts {
-    // Apply all filters *except* the one being queried (so we get a count of all studies that would be included
-    // if we allow a particular value for this category-subcategory pair)
-    var tweakedFilters = this.forceIncludeInSampleFilters(filters, category);
-    var matches = this.getUnifiedMatchesSync(tweakedFilters);
-
-    // Simulate ElasticSearch-like aggregation
-    let result = {};
-    for (let studyMatch of matches) {
-      for (let sampleMatch of studyMatch.sampleMatches) {
-        let sample = sampleMatch.sample;
-        var value = '' + (sample._source[category] || NULL_CATEGORY_NAME);
-        result[value] = (result[value] || 0) + 1;
-      }
-    }
-
-    return result;
-  }
-
-  private debugUnifiedMatches(label: string, matchesSoFar: UnifiedMatch[]): void {
-    /*
-    console.log(label);
-    console.log(JSON.stringify(matchesSoFar.map(studyMatch => {
-      var x = _.clone(studyMatch);
-      delete x.study;
-      x.sampleMatches = x.sampleMatches.map(sampleMatch => {
-        var y = _.clone(sampleMatch);
-        delete y.sample;
-        return y;
-      });
-      return x;
-    })));
-    */
-  }
-
-  private getUnifiedMatchesSync(filters: FiltersState): UnifiedMatch[] {
-
-    // 0. Start by including everything
-    let result: UnifiedMatch[] = this.getStudies().map(study => <UnifiedMatch>{
-      studyId: study.id,
-      study: study,
-      sampleMatches: (
-        this.getSamples()
-          .filter(sample => sample.studyId == study.id)
-          .map(sample => <SampleMatch>{
-            sampleId: sample.id,
-            sample: sample
-          })
-      )
-    });
-    this.debugUnifiedMatches('After 0:', result);
-
-    // 1. Annotate samples and studies according to whether their metadata matches the search text.
-    let q = filters.searchText.toLocaleLowerCase();
-    result.forEach(studyMatch => {
-      studyMatch.isFullTextMatch = (q === '') || (this.deepFind(studyMatch.study, q, STUDY_METADATA_SEARCH_FIELDS));
-      studyMatch.sampleMatches.forEach(sampleMatch => {
-        sampleMatch.isFullTextMatch = (q === '') || (this.deepFind(sampleMatch.sample, q));
-      })
-    });
-    this.debugUnifiedMatches('After 1:', result);
-
-    // 2. Apply sample metadata exclusion filters
-    result.forEach(studyMatch => {
-      studyMatch.sampleMatches.forEach(sampleMatch => {
-        sampleMatch.passesMetadataFilters = true;
-      });
-    });
-    _.forOwn(filters.sampleFilters, (sampleFilter, category) => {
-      result.forEach(studyMatch => {
-        studyMatch.sampleMatches.forEach(sampleMatch => {
-          if (this.shouldSampleBeExcluded(category, sampleFilter, sampleMatch.sample)) {
-            sampleMatch.passesMetadataFilters = false;
-          }
-        });
-      });
-    });
-    this.debugUnifiedMatches('After 3:', result);
-
-    // 3. Mark samples preliminarily as "matches" if
-    // 1) the sample hasn't been excluded by a metadata filter
-    // AND 2) the study has a full-text match or the sample has a full-text match
-    result.forEach(studyMatch => {
-      studyMatch.sampleMatches.forEach(sampleMatch => {
-        sampleMatch.isMatch = (
-          sampleMatch.passesMetadataFilters &&
-          (studyMatch.isFullTextMatch || sampleMatch.isFullTextMatch)
-        );
-      })
-    });
-    this.debugUnifiedMatches('After 4:', result);
-
-    // 4. Further match control samples of "matching" samples
-    if (filters.includeControls) {
-      result.forEach(studyMatch => {
-        var extraControlSampleNames = new Set<string>();
-        studyMatch.sampleMatches.forEach(sampleMatch => {
-          if (sampleMatch.isMatch && ('Sample Match' in sampleMatch.sample._source)) {
-            extraControlSampleNames.add(sampleMatch.sample._source['Sample Match']);
-          }
-        });
-
-        studyMatch.sampleMatches.forEach(sampleMatch => {
-          if (extraControlSampleNames.has(sampleMatch.sample._source['Sample Name'])) {
-            sampleMatch.isMatch = true;
-          }
-        });
-      });
-      this.debugUnifiedMatches('After 5:', result);
-    }
-
-    // 6. Mark studies as matches if they pass study metadata filters and either have a full-text search match
-    // or have a matching sample
-    result.forEach(studyMatch => {
-      studyMatch.isMatch = (
-        (studyMatch.isFullTextMatch || studyMatch.sampleMatches.some(sampleMatch => sampleMatch.isMatch))
-      );
-    });
-    this.debugUnifiedMatches('After 6:', result);
-
-    // 7. Filter out all non-matching studies & samples
-    result.forEach(studyMatch => {
-      studyMatch.sampleMatches = studyMatch.sampleMatches.filter(sampleMatch => sampleMatch.isMatch);
-    });
-    result = result.filter(studyMatch => studyMatch.isMatch && studyMatch.sampleMatches.length > 0);
-    this.debugUnifiedMatches('After 7:', result);
-
-    // Summary:
-    // - First include everything that matches full-text queries
-    // - Further include control samples of matching samples
-    // - Then impose study and sample metadata exclusions
-
-    return result;
-  }
-
-  private deepFind(target: (Object|Array<any>), searchText: string, onlyForKeys?: Set<string>): boolean {
-    var
-      key: string,
-      value: any
-    ;
-    for (key in target) {
-      if (target.hasOwnProperty(key)) {
-        value = target[key];
-        switch (typeof value) {
-          case 'object':
-            if (this.deepFind(value, searchText, onlyForKeys)) {
-              return true;
-            }
-            break;
-          case 'string':
-          case 'number':
-            if ((!onlyForKeys || onlyForKeys.has(key)) && ('' + value).toLocaleLowerCase().indexOf(searchText) != -1) {
-              return true;
-            }
-        }
-      }
-    }
-    return false;
   }
 }
