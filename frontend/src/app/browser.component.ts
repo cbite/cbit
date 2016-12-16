93af42b8de3516543efc21faae0c84ef5651df3c
Patrick Varilly <patrick@varilly-sciences.eu>
2016-12-16 13:23:49 +0100

    Merged template and CSS into browser.component.ts

--- frontend/src/app/browser.component.ts
+++ frontend/src/app/browser.component.ts
@@ -26,8 +26,293 @@ export const KEYS_IN_MINI_SUMMARY = {
 
 @Component({
   selector: 'browser',
-  templateUrl: './browser.component.html',
-  styleUrls: ['./browser.component.css']
+  template: `
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-3 sidebar">
+          <filter-sidebar [allFieldsModal]="allFieldsModal"></filter-sidebar>
+        </div>
+    
+        <div class="col-xs-9 col-xs-offset-3 main">
+    
+          <div class="h1">
+            Results
+            <spinner *ngIf="!ready"></spinner>
+            <button class="btn btn-success addAllResultsButton" (click)="addAllResultsToSelection()">
+              <span class="glyphicon glyphicon-plus"></span> Select All Results
+            </button>
+          </div>
+          <div class="resultsCounts">
+            {{ numMatchingStudies }} studies, {{ numMatchingSamples }} samples
+          </div>
+    
+          <div class="row">
+    
+            <!-- Have to expand *ngFor manually here to insert clearfix every 3 items -->
+            <template ngFor let-studyMatch [ngForOf]="matches" let-i="index">
+              <div class="col-md-4">
+    
+                <div class="panel"
+                     [class.panel-default]="studySelectedState(studyMatch) === 'no'"
+                     [class.panel-success]="studySelectedState(studyMatch) === 'yes'"
+                     [class.panel-warning]="studySelectedState(studyMatch) === 'partial'"
+                >
+                  <div class="panel-heading">
+    
+                    <a (click)="$event.preventDefault(); detailModal.show()" href="#">
+                      <h3 class="panel-title" style="float:left;width:80%">{{ studyMatch.study._source['STUDY']['Study Title'] }}</h3>
+                    </a>
+                    <div *ngIf="!studyMatch.study._source['*Visible']" class="embargo">[Invisible to public!]</div>
+    
+                    <div [ngSwitch]="studySelectedState(studyMatch)" class="panel-heading-plus-minus-buttons">
+    
+                      <a *ngSwitchCase="'no'" href="#" class="btn btn-success btn-xs" role="button"
+                         (click)="$event.preventDefault(); selectStudy(studyMatch)">
+                        <span class="glyphicon glyphicon-plus"></span>
+                      </a>
+    
+                      <a *ngSwitchCase="'partial'" href="#" class="btn btn-success btn-xs" role="button"
+                         (click)="$event.preventDefault(); selectStudy(studyMatch)">
+                        <span class="glyphicon glyphicon-plus"></span>
+                      </a>
+                      <a *ngSwitchCase="'partial'" href="#" class="btn btn-danger btn-xs" role="button"
+                         (click)="$event.preventDefault(); deselectStudy(studyMatch)">
+                        <span class="glyphicon glyphicon-minus"></span>
+                      </a>
+    
+                      <a *ngSwitchCase="'yes'" href="#" class="btn btn-danger btn-xs" role="button"
+                         (click)="$event.preventDefault(); deselectStudy(studyMatch)">
+                        <span class="glyphicon glyphicon-minus"></span>
+                      </a>
+    
+                    </div>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="panel-body">
+                    <p>by {{ studyMatch.study._source['STUDY']['Study Researchers Involved'] }}</p>
+    
+                    <a (click)="setSamplesShown(studyMatch.study._id, !areSamplesShown(studyMatch.study._id))" href="javascript:void(0)">
+                      <span *ngIf="!areSamplesShown(studyMatch.study._id)" class="glyphicon glyphicon-triangle-right"></span>
+                      <span *ngIf=" areSamplesShown(studyMatch.study._id)" class="glyphicon glyphicon-triangle-bottom"></span>
+                      {{ studyMatch.sampleMatches.length }} matching samples
+                      ({{ countMatchingSamplesInSelection(studyMatch) }} selected)
+                    </a>
+    
+                    <div *ngIf="areSamplesShown(studyMatch.study._id)">
+                      <ul style="list-style: none">
+                        <li *ngFor="let sampleMatch of sortSampleMatches(studyMatch.sampleMatches)">
+                          <div class="showonhover">
+    
+                            <a *ngIf="!isSampleSelected(studyMatch.study._id, sampleMatch._id)" href="#"
+                               class="btn btn-success btn-xs" role="button"
+                               (click)="$event.preventDefault(); selectSample(studyMatch.study._id, sampleMatch._id)">
+                              <span class="glyphicon glyphicon-plus"></span>
+                            </a>
+    
+                            <a *ngIf="isSampleSelected(studyMatch.study._id, sampleMatch._id)" href="#"
+                               class="btn btn-danger btn-xs" role="button"
+                               (click)="$event.preventDefault(); deselectSample(studyMatch.study._id, sampleMatch._id)">
+                              <span class="glyphicon glyphicon-minus"></span>
+                            </a>
+    
+                            <b>{{ sampleMatch._source['Sample Name'] }}</b>
+                            <span *ngFor="let kv of filteredDistinctKeyValues(studyMatch.study._id, sampleMatch) | mapToIterable; let isLast = last">
+                              <i>{{ kv.key }}</i>: {{ kv.val }}<span *ngIf="!isLast">, </span>
+                            </span>
+    
+                            <div class="hovertext">
+                              <div *ngFor="let kv of distinctKeyValues(studyMatch.study._id, sampleMatch) | mapToIterable">
+                                <b>{{ kv.key }}</b>: {{ kv.val }}
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="panel-footer">
+                    <div class="btn-group btn-group-xs" style="float:right">
+                      <a *ngFor="let pubmedId of pubmedIdsOf(studyMatch.study)"
+                         target="_blank"
+                         class="btn btn-default" role="button"
+                         pubmed-link [pubmedId]="pubmedId">
+                        PubMed
+                      </a>
+    
+                      <a *ngFor="let doi of doisOf(studyMatch.study)"
+                         target="_blank"
+                         class="btn btn-default" role="button"
+                         doi-link [doi]="doi">
+                        DOI
+                      </a>
+    
+                      <a [href]="studyMatch.study._source['*Archive URL']"
+                         class="btn btn-default" role="button">
+                        <span class="glyphicon glyphicon-download-alt"></span>
+                        Download
+                      </a>
+                    </div>
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
+    
+              </div>
+    
+              <div bsModal #detailModal="bs-modal" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+    
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" (click)="detailModal.hide()">&times;</button>
+                      <h4 class="modal-title">{{ studyMatch.study._source['STUDY']['Study Title'] }}</h4>
+                    </div>
+                    <div class="modal-body">
+                      <study [studyId]="studyMatch.study._id"></study>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" (click)="detailModal.hide()">Close</button>
+                    </div>
+                  </div>
+    
+                </div>
+              </div>
+    
+    
+              <div *ngIf="((i + 1) % 3) === 0" class="clearfix"></div>
+            </template>
+          </div>
+    
+        </div>
+    
+        <div class="col-xs-3 footer-fixed-bottom nav">
+          <li>
+            <a href="javascript:void(0)" (click)="clearFilters()">
+              <span class="glyphicon glyphicon-ban-circle"></span> Clear Filters
+            </a>
+          </li>
+        </div>
+      </div>
+    
+    
+      <div bsModal #allFieldsModal="bs-modal" class="modal fade" role="dialog" (onShow)="allFields.refresh()">
+        <div class="modal-dialog">
+    
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" (click)="allFieldsModal.hide()">&times;</button>
+              <h4 class="modal-title">Full list of fields</h4>
+            </div>
+            <div class="modal-body">
+              <all-fields #allFields="allFields"></all-fields>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" (click)="allFieldsModal.hide()">Close</button>
+            </div>
+          </div>
+    
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .showonhover .hovertext {
+      display: none;
+      position:relative;
+      font-size: 10px;
+      left: 20%;
+      z-index: 999;
+      background:#e0e0e0;
+      padding:0px 7px;
+      border: 1px solid #c0c0c0;
+      box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.4);
+    
+      opacity: 0;
+      transition:opacity 0.4s ease-out;
+    }
+    .showonhover:hover .hovertext {
+      position: absolute;
+      display: block;
+      opacity: 1;
+    }
+    
+    /*
+     * Sidebar
+     */
+    
+    .sidebar {
+      position: fixed;
+      top: 51px;
+      bottom: 0;
+      margin-bottom: 60px;
+      left: 0;
+      z-index: 1000;
+      display: block;
+      padding: 10px;
+      overflow-x: hidden;
+      overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+      background-color: #f5f5f5;
+      border-right: 1px solid #eee;
+    }
+    
+    /*
+     * Footer
+     */
+    
+    .footer-fixed-bottom {
+      position: fixed;
+      bottom: 0px;
+      height: 60px;
+      left: 0;
+      z-index: 1001;
+      display: block;
+      padding: 10px;
+      overflow-x: hidden;
+      overflow-y: hidden;
+      background-color: #f5f5f5;
+      border-top: 1px solid #eee;
+      border-right: 1px solid #eee;
+    }
+    
+    /*
+     * Main content
+     */
+    
+    .main {
+      padding-right: 20px;
+      padding-left: 20px;
+    }
+    .main .page-header {
+      margin-top: 0;
+    }
+    
+    .resultsCounts {
+      font-size: 90%;
+      margin-bottom: 20px;
+      font-style: oblique;
+    }
+    
+    .addAllResultsButton {
+      float: right;
+      margin-top: 20px;
+    }
+    
+    /*
+     * Corrections for panel headings
+     */
+    div.panel-heading {
+      position: relative;
+    }
+    
+    div.panel-heading-plus-minus-buttons {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+    
+    div.panel-heading embargo {
+      font-style: italic;
+    }
+  `]
 })
 export class BrowserComponent implements OnInit, OnDestroy {
 
