e375286d290abb57de6fc1cbaadea076250e9e15
Patrick Varilly <patrick.varilly@dataminded.be>
2017-07-18 11:54:45 +0200

    Final cleanup

--- frontend/src/app/about.component.ts
+++ frontend/src/app/about.component.ts
@@ -6,15 +6,16 @@ import {CollapseStateService} from "./services/collapse-state.service";
     <div class="container">
       <div class="row justified">
         <div class="col-xs-8 col-xs-offset-2">
-        
+
           <h3>What is cBiT?</h3>
-    
+
           <p>
             cBiT is the first repository that offers biomaterial-based transcriptomics
             data together with all relevant biomaterial metadata.
-            
+
             cBiT was developed at the department of <a href="http://www.jandeboerlab.com">Cell Biology-Inspired Tissue
-            Engineering (cBITE)</a>, part of the <a href="http://merln.maastrichtuniversity.nl/">MERLN Institute at Maastricht University</a>.
+            Engineering (cBITE)</a>, part of the <a href="http://merln.maastrichtuniversity.nl/">MERLN Institute at
+            Maastricht University</a>.
           </p>
 
           <h3>Why cBiT?</h3>
@@ -23,43 +24,43 @@ import {CollapseStateService} from "./services/collapse-state.service";
             <li>cBiT is an important step towards standardization in the field of cell-biomaterial interaction</li>
             <li>in depth (meta)analysis of cBiT’s big data will accelerate biomaterial development</li>
           </ul>
-          
+
           <img src="/public/images/cbit-overview-final.png" class="img-overview">
-          
+
           <h3>What can we do for you?</h3>
-          
+
           <p>
             The possibilities offered by cBiT are:
           </p>
-          
+
           <ol class="possibilities">
             <li>
               <div class="possibility">
                 Browse, download and analyze data yourself
               </div>
-              
+
               cBiT can be easily queried under <a routerLink="/browse">Browse</a> using either free text search or
               a material and study property quick selection menu. Studies or samples of interest can be selected
               and subsequently downloaded. For a tutorial on how to use cBiT, please have a look at
               <a href="/public/pdfs/cBiT-user-manual.pdf">this pdf</a>.
             </li>
-            
+
             <li>
               <div class="possibility">
                 Browse, download and find an external party to analyze the data
               </div>
-              
+
               cBiT can be easily queried under Browse using either free text search or a material and study property
               quick selection menu. Studies or samples of interest can be selected and subsequently downloaded. For a
               tutorial on how to use cBiT, please have a look at
               <a href="/public/pdfs/cBiT-user-manual.pdf">this pdf</a>.
             </li>
-            
+
             <li>
               <div class="possibility">
                 Contact us with an interesting question and ask us to analyze cBiT data
               </div>
-              
+
               We have ample experience with materiomics-data analysis and are happy to help out! Researchers who are
               interested in analyzing cBiT data can contact us for support, please send an email to Dennie Hebels
               (<code>d.hebels [at] maastrichtuniversity.nl</code>).
@@ -75,19 +76,19 @@ import {CollapseStateService} from "./services/collapse-state.service";
               can contact us for support, please send an email to Dennie Hebels
               (<code>d.hebels [at] maastrichtuniversity.nl</code>).
             </li>
-            
+
             <li>
               <div class="possibility">
                 Upload your own dataset
               </div>
-              
+
               Researchers who are interested in depositing their own data in cBiT can contact us for detailed
               instructions and support on how to prepare the data into archives. Please send an email to Dennie
               Hebels (<code>d.hebels [at] maastrichtuniversity.nl</code>).
             </li>
           </ol>
 
-          
+
           <h3>Want to know more?</h3>
           <p>
             More details on how cBiT was designed and the principles of materiomics can be found in
@@ -95,15 +96,15 @@ import {CollapseStateService} from "./services/collapse-state.service";
           </p>
           <ul>
             <li>
-              Hebels et al.. cBiT: A Transcriptomics Database for Innovative Biomaterial Engineering.
+              Hebels et al., cBiT: A Transcriptomics Database for Innovative Biomaterial Engineering.
               [Submitted]
             </li>
-            
+
             <li>
               Cranford SW, et al., Materiomics: An -omics Approach to Biomaterials Research.
               Adv Mater, 25: 802–824 (2013).
             </li>
-            
+
             <li>
               de Boer J and van Blitterswijk CA (eds.), Materiomics – High throughput screening of
               biomaterial properties. Cambridge University Press 2013.
@@ -119,32 +120,32 @@ import {CollapseStateService} from "./services/collapse-state.service";
             6229 ER Maastricht<br/>
             The Netherlands
           </div>
-          
+
           <p>
-          Questions regarding the use of cBiT or other details on how cBiT is set up, can be
-          directed to Dennie Hebels (<code>d.hebels [at] maastrichtuniversity.nl</code>).
+            Questions regarding the use of cBiT or other details on how cBiT is set up, can be
+            directed to Dennie Hebels (<code>d.hebels [at] maastrichtuniversity.nl</code>).
           </p>
-          
-          <div class="smaller">
-            <h5>Licensing information</h5> 
+
+          <div class="disclaimers">
+            <h5>Licensing information</h5>
             <p>
-            The use of cBiT is free of charge for academic users. Commercial users should
-            contact Prof. Jan de Boer (<code>jan.deboer [at] maastrichtuniversity.nl</code>).
-            </p> 
-            
-            <p>
-            If you would like written approval for a specific use that is otherwise prohibited,
-            please contact Dr. Dennie Hebels (<code>d.hebels [at] maastrichtuniversity.nl</code>).
+              The use of cBiT is free of charge for academic users. Commercial users should
+              contact Prof. Jan de Boer (<code>jan.deboer [at] maastrichtuniversity.nl</code>).
             </p>
-            
+
+            <p>
+              If you would like written approval for a specific use that is otherwise prohibited,
+              please contact Dr. Dennie Hebels (<code>d.hebels [at] maastrichtuniversity.nl</code>).
+            </p>
+
             <h5>Disclaimer</h5>
             <p>
-            The information, services or products provided on cBiT may contain errors and may
-            be subject to temporary interruption. Although we do our best to provide all
-            information, services and products as complete and accurate as possible, we cannot
-            be held responsible for any errors, defects or other harmful consequences, deriving
-            from the use of cBiT. Therefore we renounce any liability whatsoever, within the
-            boundaries of Dutch law.
+              The information, services or products provided on cBiT may contain errors and may
+              be subject to temporary interruption. Although we do our best to provide all
+              information, services and products as complete and accurate as possible, we cannot
+              be held responsible for any errors, defects or other harmful consequences, deriving
+              from the use of cBiT. Therefore we renounce any liability whatsoever, within the
+              boundaries of Dutch law.
             </p>
           </div>
 
@@ -160,7 +161,7 @@ import {CollapseStateService} from "./services/collapse-state.service";
     .justified {
       text-align: justify;
     }
-    
+
     .img-overview {
       display: block;
       text-align: center;
@@ -168,29 +169,33 @@ import {CollapseStateService} from "./services/collapse-state.service";
       margin-top: 15px;
       margin-bottom: 40px
     }
-    
+
     .contact-details {
       margin-left: 40px;
       margin-top: 20px;
       margin-bottom: 20px;
     }
 
-    .smaller {
-      margin-top: 30px;
+    .disclaimers {
+      margin-top: 60px;
       font-size: 80%;
     }
     
+    .disclaimers > h5 {
+      font-weight: bolder;
+    }
+
     .copyright {
       width: 100%;
       text-align: center;
       margin-top: 30px;
       font-size: 80%;
     }
-    
+
     .possibilities > li {
       margin-top: 10px;
     }
-    
+
     .possibility {
       font-weight: bold;
     }
--- frontend/src/app/welcome.component.ts
+++ frontend/src/app/welcome.component.ts
@@ -35,27 +35,6 @@ import {Component} from '@angular/core';
             part of the MERLN Institute at Maastricht University.
           </p>
           
-          <!--
-          <p>
-          OLD OLD OLD: cBiT is a repository that incorporates material science and transcriptomics-based
-          cell biology, with a focus on clinically relevant materials. cBiT was developed at
-		  the department of Cell Biology-Inspired Tissue Engineering (cBITE), part of the MERLN
-		  Institute at Maastricht University. Check out the About section for more information
-		  or go directly to Browse to see what we have to offer!
-          </p> 
-          
-          <img src="/public/images/cbit-overview.png" class="img-overview"> 
-          
-          <div class="centered">
-            <iframe width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/Zvvaz1qJW0s"
-                    frameborder="0"
-                    allowfullscreen>
-            </iframe>
-          </div>
-          -->
-
           <p class="copyright">
             &copy; 2017 Laboratory for cell-Biology inspired Tissue Engineering, Maastricht University. 
             All Rights Reserved.
