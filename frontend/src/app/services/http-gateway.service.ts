310ea0367c16fb3c953ee9f4b8ab4b97905d5ec6
maartenthoelen <maarten.thoelen@gmail.com>
2018-07-09 10:47:12 +0200

    Headers fix

--- frontend/src/app/services/http-gateway.service.ts
+++ frontend/src/app/services/http-gateway.service.ts
@@ -7,112 +7,106 @@ import {environment} from '../../environments/environment';
 
 @Injectable()
 export class HttpGatewayService {
-    private headers: HttpHeaders;
+  private headers: HttpHeaders;
 
-    constructor(private http: HttpClient,
-                private authenticationService: AuthenticationService,
-                private busyIndicatorService: BusyIndicatorService) {
-        this.headers = new HttpHeaders({
-            'Content-Type': 'application/json'
-        });
-    }
+  constructor(private http: HttpClient,
+              private authenticationService: AuthenticationService,
+              private busyIndicatorService: BusyIndicatorService) {
+  }
 
-    public get(url: string, actionKey?: string, headers?: HttpHeaders): Observable<any> {
-        this.trySetBusy(actionKey);
-        return this.http.get(this.resolveUrl(url), {
-            headers: headers ? headers : this.withAuthHeader(this.headers),
-            withCredentials: true
-        })
-            .map(this.checkResponseBody.bind(this))
-            .catch(this.handleError)
-            .finally(() => this.trySetIdle(actionKey));
-    }
+  public get(url: string, actionKey?: string, headers?: HttpHeaders): Observable<any> {
+    this.trySetBusy(actionKey);
+    return this.http.get(this.resolveUrl(url), {
+      headers: headers ? headers : this.withAuthHeader(),
+      withCredentials: true
+    })
+      .map(this.checkResponseBody.bind(this))
+      .catch(this.handleError)
+      .finally(() => this.trySetIdle(actionKey));
+  }
 
-    public delete(url: string, actionKey?: string, headers?: HttpHeaders): Observable<any> {
-        this.trySetBusy(actionKey);
-        return this.http.delete(this.resolveUrl(url), {
-            headers: headers ? headers : this.withAuthHeader(this.headers),
-            withCredentials: true
-        })
-            .map(this.checkResponseBody.bind(this))
-            .catch(this.handleError)
-            .finally(() => this.trySetIdle(actionKey));
-    }
+  public delete(url: string, actionKey?: string, headers?: HttpHeaders): Observable<any> {
+    this.trySetBusy(actionKey);
+    return this.http.delete(this.resolveUrl(url), {
+      headers: headers ? headers : this.withAuthHeader(),
+      withCredentials: true
+    })
+      .map(this.checkResponseBody.bind(this))
+      .catch(this.handleError)
+      .finally(() => this.trySetIdle(actionKey));
+  }
 
-    public post(url: string, body: any, actionKey?: string, headers?: HttpHeaders): Observable<any> {
-        this.trySetBusy(actionKey);
-        return this.http.post(this.resolveUrl(url), body, {
-            headers: headers ? headers : this.withAuthHeader(this.headers),
-            withCredentials: true
-        })
-            .map(this.checkResponseBody.bind(this))
-            .catch(this.handleError)
-            .finally(() => this.trySetIdle(actionKey));
-    }
+  public post(url: string, body: any, actionKey?: string, headers?: HttpHeaders): Observable<any> {
+    this.trySetBusy(actionKey);
+    return this.http.post(this.resolveUrl(url), body, {
+      headers: headers ? headers : this.withAuthHeader(),
+      withCredentials: true
+    })
+      .map(this.checkResponseBody.bind(this))
+      .catch(this.handleError)
+      .finally(() => this.trySetIdle(actionKey));
+  }
 
-    public put(url: string, body: any, actionKey?: string, headers?: HttpHeaders): Observable<any> {
-        this.trySetBusy(actionKey);
-        return this.http.put(this.resolveUrl(url), body, {
-            headers: headers ? headers : this.withAuthHeader(this.headers),
-            withCredentials: true
-        })
-            .map(this.checkResponseBody.bind(this))
-            .catch(this.handleError)
-            .finally(() => this.trySetIdle(actionKey));
-    }
+  public put(url: string, body: any, actionKey?: string, headers?: HttpHeaders): Observable<any> {
+    this.trySetBusy(actionKey);
+    return this.http.put(this.resolveUrl(url), body, {
+      headers: headers ? headers : this.withAuthHeader(),
+      withCredentials: true
+    })
+      .map(this.checkResponseBody.bind(this))
+      .catch(this.handleError)
+      .finally(() => this.trySetIdle(actionKey));
+  }
 
-    private withAuthHeader(headers: HttpHeaders): HttpHeaders {
-        const authHeader = headers.get('Authorization');
-        if (!authHeader || authHeader === '') {
-           const authHeaderContent = this.authenticationService.getAuthorizationHeader();
-            if (authHeaderContent) {
-                headers.append('Authorization', authHeaderContent);
-            }
-        }
-        return headers;
-    }
+  private withAuthHeader(): HttpHeaders {
+    const authHeaderContent = this.authenticationService.getAuthorizationHeader();
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': authHeaderContent
+    });
+  }
 
-    private resolveUrl(url: string) {
-        return environment.api_url + url;
-    }
+  private resolveUrl(url: string) {
+    return environment.api_url + url;
+  }
 
-    private trySetBusy(actionKey?: string) {
-        if (actionKey) {
-            this.busyIndicatorService.setBusy(actionKey);
-        }
+  private trySetBusy(actionKey?: string) {
+    if (actionKey) {
+      this.busyIndicatorService.setBusy(actionKey);
     }
+  }
 
-    private trySetIdle(actionKey?: string) {
-        if (actionKey) {
-            this.busyIndicatorService.setIdle(actionKey);
-        }
+  private trySetIdle(actionKey?: string) {
+    if (actionKey) {
+      this.busyIndicatorService.setIdle(actionKey);
     }
+  }
 
-    private checkResponseBody(body: any): any {
-        // throw error when server returned error
-        if (body.error) {
-            if (body.error.status === 401) {
-                this.handleUnauthorized();
-            } else {
-                throw new Error(body.error.message);
-            }
-        } else {
-            if (body.data) {
-                return body.data;
-            } else {
-                return body;
-            }
-        }
+  private checkResponseBody(body: any): any {
+    // throw error when server returned error
+    if (body.error) {
+      if (body.error.status === 401) {
+        this.handleUnauthorized();
+      } else {
+        throw new Error(body.error.message);
+      }
+    } else {
+      if (body.data) {
+        return body.data;
+      } else {
+        return body;
+      }
     }
+  }
 
-    private handleUnauthorized() {
-        // delete the authorization header
-        this.headers.delete('Authorization');
-    }
+  private handleUnauthorized() {
+    // delete the authorization header
+    this.headers.delete('Authorization');
+  }
 
-    private handleError<T>(error: any) {
-        const errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'An error occurred on the server.';
-        return Observable.throw(errMsg);
-    }
+  private handleError<T>(error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'An error occurred on the server.';
+    return Observable.throw(errMsg);
+  }
 }
