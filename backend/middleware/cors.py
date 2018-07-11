91aea68002bf370c2da9da3ea3f7989d70258826
maartenthoelen <maarten.thoelen@gmail.com>
2018-07-11 12:25:46 +0200

    Reset cors

--- backend/middleware/cors.py
+++ backend/middleware/cors.py
@@ -3,7 +3,7 @@ import falcon_cors
 def CORSMiddleware():
     """See https://pypi.python.org/pypi/falcon-cors for CORS info"""
 
-    cors_origins = ['*']
+    cors_origins = ['http://localhost:8080']
     cors = falcon_cors.CORS(
         allow_origins_list=cors_origins,
         allow_credentials_origins_list=cors_origins,
