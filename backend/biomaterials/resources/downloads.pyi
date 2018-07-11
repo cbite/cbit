c3cb4bfd37ebc2ffe2cd95689d9d1805d45aa985
maartenthoelen <maarten.thoelen@gmail.com>
2018-07-11 08:57:48 +0200

    Restructuring

--- backend/biomaterials/resources/downloads.pyi
+++
@@ -1,7 +0,0 @@
-import falcon
-
-class DownloadsResource(object):
-    def on_put(self, req: falcon.Request, resp: falcon.Response): ...
-
-class DownloadResource(object):
-    def on_get(self, req: falcon.Request, resp: falcon.Response, download_uuid: str): ...
