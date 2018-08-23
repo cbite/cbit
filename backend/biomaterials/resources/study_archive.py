6c6fd33942e0ad1ddcd40ffe9e68c70e3d994736
maartenthoelen <maarten.thoelen@gmail.com>
2018-08-23 16:50:05 +0200

    Add extra file to download of study

--- backend/biomaterials/resources/study_archive.py
+++ backend/biomaterials/resources/study_archive.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import zipfile
+
 import config.config as cfg
 import falcon
 import elasticsearch
@@ -24,13 +27,64 @@ class BiomaterialsStudyArchiveResource(object):
                     description="Study ID {0} doesn't exist".format(
                         study_uuid))
 
-        ingested_archive_path = os.path.join(cfg.FILES_PATH, study_uuid,
-                                             'archive.zip')
+        ingested_archive_path = os.path.join(cfg.FILES_PATH, study_uuid,'archive.zip')
 
-        if os.path.exists(ingested_archive_path):
+        download_archive_path = createDownloadArchive(ingested_archive_path)
+
+        if os.path.exists(download_archive_path):
             resp.content_type = 'application/zip'
             resp.append_header("Content-Disposition", 'attachment; filename="study.zip"')
-            resp.stream = open(ingested_archive_path, 'rb')
-            resp.stream_len = os.path.getsize(ingested_archive_path)
+            resp.stream = open(download_archive_path, 'rb')
+            resp.stream_len = os.path.getsize(download_archive_path)
         else:
             raise falcon.HTTPNotFound(description="No archive found for study id {0}".format(study_uuid))
+
+def createDownloadArchive(zip_file_path):
+    zip_name = os.path.basename(zip_file_path)
+    zip_dir = os.path.dirname(zip_file_path)
+
+    download_dir = os.path.join(zip_dir, 'download')
+
+    if os.path.exists(download_dir):
+        # If download dir exists -> clear it
+        for the_file in os.listdir(download_dir):
+            file_path = os.path.join(download_dir, the_file)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    else:
+         # If not -> create it
+         os.makedirs(download_dir)
+
+    # Create a temporary directory in the download dir
+    temp_dir = os.path.join(download_dir, 'temp')
+    os.makedirs(temp_dir)
+
+    # Temporarily create a copy of the zip archive
+    shutil.copy(zip_file_path, temp_dir)
+    temp_zip_file_path = os.path.join(temp_dir, zip_name)
+    download_zip_file_path = os.path.join(download_dir, zip_name)
+
+    # Create a dir for the temporary archive content
+    temp_zip_content_dir = os.path.join(temp_dir, 'arch')
+    os.makedirs(temp_zip_content_dir)
+
+    # Unzip the temporary archive
+    temp_zip_ref = zipfile.ZipFile(temp_zip_file_path, 'r')
+    temp_zip_ref.extractall(temp_zip_content_dir)
+    temp_zip_ref.close()
+
+    # Remove temporary zip archive
+    os.remove(temp_zip_file_path)
+
+    # Add the extra file
+    detail_overview_file_path = os.path.join(cfg.COMMON_FILES_PATH,'Study detail and property overview_v9.xlsx')
+    shutil.copy(detail_overview_file_path, temp_zip_content_dir)
+
+    # Create new archive
+    shutil.make_archive(download_zip_file_path.strip('.zip'), 'zip', temp_zip_content_dir)
+
+    # Clean up temporary directory
+    shutil.rmtree(temp_dir)
+
+    return download_zip_file_path
